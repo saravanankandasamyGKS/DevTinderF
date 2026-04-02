c5a41a94740b8cdfeaf175b36652d240139f74a8
saravanankandasamyGKS <147834306+saravanankandasamyGKS@users.noreply.github.com>
2026-04-02 09:57:13 +0530

    Refactor handleSendRequest for improved error handling
    Updated handleSendRequest to log response data and handle errors more gracefully.

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -12,13 +12,19 @@ const UserCard = ({ user }) => {
 
   console.log("Extracted Skills:", skills); // Debugging
 
- const handleSendRequest = async (status, userId) => { try 
- {
-   const res = await axios.post( BASE_URL+"/request/send/"+status+"/"+ userId, {},{ withCredentials: true, } ); 
-   dispatch(removeUserFromFeed(userId)); 
- } catch (error) {
-   console.log(error); 
- }
+ const handleSendRequest = async (status, userId) => {
+  try {
+    const res = await axios.post(
+      `http://localhost:5000/request/send/${status}/${userId}`,
+      {},
+      { withCredentials: true }
+    );
+
+    console.log(res.data);
+  } catch (err) {
+    console.log(err.response?.data);
+  }
+};
                                                      };
   return (
     <div className="card grid-rows-1 bg-base-300 w-96 shadow-xl p-3">
